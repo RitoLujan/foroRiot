c493a79a469ecbb94a6f28e759111b39406aac2f
Joel Humberto Terrones Lujan <157426275+RitoLujan@users.noreply.github.com>
2024-07-15 22:41:37 -0600

    primer comit del foro para el challenge

---
+++ api/src/main/java/med/voll/api/domain/medico/DatosActualizarMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.domain.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.DatosDireccion;
+
+public record DatosActualizarMedico(
+        @NotNull
+        Long id,
+        String nombre,
+        String documento,
+        DatosDireccion direccion
+) {
+}
---
+++ api/src/main/java/med/voll/api/domain/medico/DatosRespuestaMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.domain.medico;
+
+import med.voll.api.DatosDireccion;
+
+public record DatosRespuestaMedico(
+        Long id,
+        String nombre,
+        String email,
+        String telefono,
+        String documento,
+        DatosDireccion direccion
+) {
+}
